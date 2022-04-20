f2195119a77821e5dee0e77eb55d7d678cbd92d5
dannyboy73 <58300207+dannyboy73@users.noreply.github.com>
2022-04-20 09:48:44 +0530

    Custom classification report function

--- covid19_cnn_v1.py
+++ covid19_cnn_v1.py
@@ -1,75 +1,108 @@
-from tensorflow.keras.preprocessing import image_dataset_from_directory
-from tensorflow.keras import layers
-from tensorflow.keras.models import Sequential
-from tensorflow import expand_dims
-from keras.preprocessing import image
-from keras.optimizers import Adam
-from keras.callbacks import LearningRateScheduler
-from keras.models import load_model
-from sklearn.metrics import classification_report, confusion_matrix
+from tensorflow.keras.preprocessing import image_dataset_from_directory # for importing images form the directory
+from tensorflow.keras.models import Sequential # used to group a stack of layers
+from tensorflow import expand_dims # to expand the shape of image array
+from keras.preprocessing import image # functions for image pre-processing
+from tensorflow.keras.models import load_model # to import the necessary model
+from tensorflow.keras.optimizers import Adam # optimization algorithm for the model
+from sklearn.metrics import confusion_matrix # metrics to evaluate the model
 import argparse
-import matplotlib.pyplot as plt
-import numpy as np
-from cv2 import imread
 import os
+import matplotlib.pyplot as plt
+from tensorflow.keras import layers
+import tensorflow as tf
+import numpy as np
 
-def predict_test_set(path, normal=True):
-        '''Used for predicting images in the test set'''
-        img = []
-        path_format = path + '{0}'
-
-        for filepath in os.listdir(path):
-                x = image.load_img(path_format.format(filepath), target_size=(256, 256),
-                        color_mode="grayscale")
-                x = image.img_to_array(x)
-                x = expand_dims(x, 0)
-                img.append(x)
-        
-        preds = []
-        for i in img:
-                predictions = model.predict(i)
-                preds.append(predictions[0][0])
-                print(predictions)
-        
-        count = 0
-        for pred in preds:
-                if normal:
-                        if pred < 0.5:
-                                count += 1
-                else:
-                        if pred >= 0.5:
-                                count += 1
-
-        return count
- 
+def classification_report(path):
+	'''Used for predicting images in the test set and find how many are classified correctly w.r.t to each class'''
+	img = []
+	y_true = np.zeros(100, dtype=int)
+	y_true = np.concatenate((y_true, np.ones(100, dtype=int)), axis=0)
+	path_format = path + '{0}'
+	ext = ['normal/', 'pneumonia/']
+	for i in ext:
+		for filepath in os.listdir(path+i):
+			x = image.load_img(path+i+filepath, target_size=(256, 256),
+					    color_mode="grayscale")
+			x = image.img_to_array(x)
+			x = expand_dims(x, 0)
+			img.append(x)
+		
+	preds = []
+	for x, i in enumerate(img):
+		predictions = model.predict(i)
+		preds.append(predictions[0][0])
+		print(predictions, y_true[x])
+		if preds[x] >= 0.5: preds[x] = 1
+		else: preds[x] = 0
+	
+	tot_images = 200
+	tp, tn, fn, fp = 0, 0, 0, 0
+	for i in range(200):
+		if preds[i] == 1 and y_true[i] == 1:
+		    tp += 1
+		if preds[i] == 0 and y_true[i] == 0:
+		    tn += 1
+		if preds[i] == 1 and y_true[i] == 0:
+		    fn += 1
+		if preds[i] == 0 and y_true[i] == 1:
+		    fp += 1
+		    
+	print("-------------------------------")
+	print("|Accuracy: ", str((tp+tn)/tot_images))
+	print("|Precision: ", str(tp/(tp+fp)))
+	print("|Recall: ", str(tp/(tp+fn)))
+	print("|F1: ", str(2*(tp/(tp+fp)*tp/(tp+fn))/(tp/(tp+fp)+tp/(tp+fn))))
+	print("-------------------------------")
+	
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-d", "--dataset", required=True,
         help="path to the input dataset")
 ap.add_argument("-m", "--model", required=True,
-        help="path to save the model")
-ap.add_argument("-o", "--output", required=True,
-        help="path to the output plot")
+        help="path to load/save the trained model")
 args = vars(ap.parse_args())
 
-# loading the dataset from disk
-print("[INFO] loading the dataset...")
+img = []
+path = []
+path.append(args["dataset"] + 'train/normal/')
+path.append(args["dataset"] + 'train/pneumonia/')
+
+# loading the train image data (80% for train, 20% for validation)
 train_dataset = image_dataset_from_directory(
         args["dataset"], labels='inferred', label_mode='binary',
-        color_mode="grayscale", image_size=(256, 256), subset="training",
+        color_mode="grayscale", image_size=(256,256), subset="training",
         seed=7, validation_split=0.2)
 
+# loading the validation image data 
 validation_dataset = image_dataset_from_directory(
-        args["dataset"], labels='inferred', label_mode='binary',
-        color_mode="grayscale", image_size=(256, 256), subset="validation",
+        args["dataset"], labels='inferred', label_mode="binary",
+        color_mode="grayscale", image_size=(256,256), subset="validation",
         seed=7, validation_split=0.2)
 
+# loading the test image data
 test_dataset = image_dataset_from_directory(
-        "xray_dataset/test/", labels='inferred', label_mode='binary',
-        color_mode='grayscale', image_size=(256, 256), seed=7)
+        "xray_dataset/test", labels='inferred', label_mode='binary',
+        color_mode="grayscale", image_size=(256,256), seed=7)
 
-# building the model. we also apply standardization within the model
+class_names = train_dataset.class_names # obtain the different classes (Normal, Pneumonia)
+print(class_names)
+
+'''
+# Image pre-processing technique used
+# 1. Data augmentation
+'''
+data_augmentation = Sequential([
+    #layers.RandomFlip("horizontal_and_vertical"),
+    layers.RandomRotation(0.2),
+    #layers.RandomContrast(0.1),
+])
+
+# Note: The other data augmentation techniques haven't helped increase the performance of the model
+
+# building the model. we apply standardization within the model
 model = Sequential([
+    # applying the data augmentation layer
+    data_augmentation,
     # standardizing the input matrix to the range [0, 1]
     layers.experimental.preprocessing.Rescaling(1./255, input_shape=(256, 256, 1)),
 
@@ -100,18 +133,18 @@ model = Sequential([
     layers.Dense(1, activation='sigmoid'),
 ])
 
+'''
 # compiling the model
 #opt = SGD(lr=0.05, decay=0.05, momentum=0.9, nesterov=True)
-opt = Adam(lr=0.0001)
+opt = Adam(learning_rate=0.0005)
 model.compile(optimizer=opt,
         loss='binary_crossentropy',
         metrics=["accuracy"])
 
-model.summary()
 
 # training the model
 print("[INFO] training the network...")
-epochs = 15
+epochs = 10
 H = model.fit(
         train_dataset,
         validation_data=validation_dataset,
@@ -119,43 +152,52 @@ H = model.fit(
         epochs=epochs,
         verbose=1,
     )
+'''
 
-# loading the pre-trained model
-#print("[INFO] loading the pre-trained model")
-#model = load_model(args["model"])
+model = load_model(args["model"])
+
+model.summary()
+
+
+#print("[INFO] saving the model...")
+#model.save(args["model"])
 
 #classification reports on training data
 
+'''
 y_true = np.concatenate([y for x, y in test_dataset], axis=0)
 y_pred = model.predict(test_dataset, batch_size=64)
 
-y_pred[y_pred >= 0.5] = 1  # converting all the positive cases
-y_pred[y_pred < 0.5] = 0  # converting all the negative cases
+y_pred[y_pred >= 0.6] = 1  # converting all the positive cases
+y_pred[y_pred < 0.6] = 0  # converting all the negative cases
 
 y_true = list(y_true.ravel())
 y_pred = list(y_pred.ravel())
 
-print(classification_report(y_true, y_pred, 
-    target_names=['normal', 'pneumonia']))
+print(y_true)
+print(y_pred)
+'''
+
+# create a function for classification_report and confusion matrix
+
+print('Classification report')
+classification_report('xray_dataset/test/')
 
 print('Confusion matrix')
-print(confusion_matrix(y_true, y_pred))
+#print(confusion_matrix(y_true, y_pred))
 
-print("[INFO] saving the model...")
-model.save(args["model"])
+'''
+plt.figure(figsize=(10, 10))
+for images, labels in train_dataset.take(1): 
+    print(images[0])
+    for i in range(9):
+        image = tf.expand_dims(images[0], 0)
+        print(image)
+        augmented_image = data_augmentation(image)
+        ax = plt.subplot(3, 3, i+1)
+        plt.imshow(augmented_image[0].numpy().astype("uint8"), cmap='gray')
+        plt.axis("off")
 
-# finding predictions for normal test images
-print('Correctly classified images(normal): ', predict_test_set('xray_dataset/test/normal/', normal=True))
-print('Correctly classified images(pneumonia): ', predict_test_set('xray_dataset/test/pneumonia/', normal=False))
+plt.show()
+'''
 
-# plotting the evaluation results
-epochs_range = range(epochs)
-plt.figure()
-plt.plot(epochs_range, H.history['accuracy'], label='Training accuracy')
-plt.plot(epochs_range, H.history['val_accuracy'], label='Validation accuracy')
-plt.plot(epochs_range, H.history['loss'], label='Training loss')
-plt.plot(epochs_range, H.history['val_loss'], label='Validation loss')
-plt.xlabel('Epoch #')
-plt.ylabel('Loss/accuracy')
-plt.legend()
-plt.savefig(args["output"])
